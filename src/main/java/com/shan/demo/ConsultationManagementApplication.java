7f8b06ec44d64d8ead09403a7944c30d9bb94ac5
[Santheep Ganeswaran] <[gshantheep@gmail.com]>
2023-08-08 20:31:00 +0530

    Initial commit

---
+++ src/main/java/com/shan/demo/ConsultationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.shan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultationManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shan/demo/ConsultationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
